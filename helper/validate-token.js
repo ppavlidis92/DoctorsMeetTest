cf5c0c7b7b769dae757cfd61ffe4c8d82ce1818e
ppavlidis92 <pavlos.pvlds@gmail.com>
2022-12-05 21:23:56 +0200

    test

--- helper/validate-token.js
+++ helper/validate-token.js
@@ -1,55 +1,54 @@
-const jwt = require('jsonwebtoken')
-var User = require('../models/users');
-
-
+const jwt = require("jsonwebtoken");
+var User = require("../models/users");
 
 // middleware to validate token (rutas protegidas)
-const verifyToken = (req, res, next) => {
-    let token
-    try {
-         token = req.cookies.authToken
-       // console.log("token "+token)
-    } catch (error) {
-        console.log(error);
-    }
-    
-   if(token){
-        jwt.verify(token,process.env.JWT_KEY,(err,decodedUser)=>{
-            if(err){
-                console.log(err);
-                res.redirect('/login')
-            }else{
-                // console.log(decodedUser);
-                let email =decodedUser.email
-                User.find({email},(err,result)=>{
-                    if(err){
-                        console.log(err);
-                         res.redirect('/login')
-                    }else{
-                        next()
-                    }
-                })
-             
-            }
-        })
-   }else{
-   
-        try {
-            res.redirect('/login')
+const verifyToken = async (req, res, next) => {
+  let token;
+  try {
+    token = req.cookies.authToken;
+    // console.log("token "+token)
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_KEY, async (err, decodedUser) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/login");
+      } else {
+        // console.log(decodedUser);
+        let email = decodedUser.email;
+
+        await User.find({ email }, (err, data) => {
+          if (err) {
+            console.log(err);
+            res.redirect("/login");
+          } else {
             next();
-        } catch (error) {
-            console.log(error);
-        }
-   }
-}
-
-
+          }
+        })
+          .clone()
+          .catch(function (err) {
+            console.log(err);
+          });
+      }
+    });
+  } else {
+    try {
+      res.redirect("/login");
+      next();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 const parseJwt = (token) => {
-	try {
-		return JSON.parse(atob(token.split('.')[1]));
-	} catch (e) {
-		return null;
-	}
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch (e) {
+    return null;
+  }
 };
 module.exports = verifyToken;
